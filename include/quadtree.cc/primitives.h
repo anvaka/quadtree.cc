1a7f713a0cdb5f6e408d02e3881e68ab69bbc703
anvaka <anvaka@gmail.com>
2016-06-12 23:16:46 -0700

    Added 2d template specialization

--- include/quadtree.cc/primitives.h
+++ include/quadtree.cc/primitives.h
@@ -17,6 +17,8 @@ struct IVector {
   virtual double operator [](size_t idx) const = 0;
 };
 
+// TODO: Rename this file from primitives to vector
+// TODO: Rename Vector3 to something else. It's no longer 3d.
 template <size_t DIMENSION>
 struct Vector3 : public IVector {
   static const int size = DIMENSION;
@@ -231,6 +233,108 @@ struct Vector3<3> : public IVector {
     return this;
   }
 };
+
+template <>
+struct Vector3<2> : public IVector {
+  static const int size = 2;
+  double coord[size];
+
+  Vector3() {
+    coord[0] = coord[1] = 0;
+  }
+
+  Vector3(const Vector3 &other) {
+    coord[0] = other.coord[0];
+    coord[1] = other.coord[1];
+  }
+
+  virtual double operator [](size_t idx) const {
+    return coord[idx];
+  }
+
+  friend Vector3 operator-(Vector3 left, const Vector3& right) {
+    Vector3 result(left);
+    result.sub(right);
+    return result;
+  }
+
+  bool isZero() {
+    return coord[0] == 0 && coord[1] == 0;
+
+  }
+
+  void reset () {
+    coord[0] = coord[1] = 0;
+  }
+
+  bool sameAs(const Vector3 &other) {
+    return std::abs(coord[0] - other.coord[0]) < 1e-8 &&
+    std::abs(coord[1] - other.coord[1]) < 1e-8;
+  }
+
+  bool operator==(const Vector3 &other) {
+    return coord[0] == other.coord[0] &&
+    coord[1] == other.coord[1];
+  }
+
+  void operator=(const Vector3 &other) {
+    coord[0] = other.coord[0];
+    coord[1] = other.coord[1];
+  }
+
+  double length() {
+    return sqrt(coord[0] * coord[0] + coord[1] * coord[1]);
+  }
+
+  Vector3* multiplyScalar(const double &scalar) {
+    coord[0] *= scalar;
+    coord[1] *= scalar;
+    return this;
+  }
+
+  Vector3* setMedian(const Vector3 min, const Vector3 max) {
+    coord[0] = (min.coord[0] + max.coord[0])/2;
+    coord[1] = (min.coord[1] + max.coord[1])/2;
+    return this;
+  }
+
+  Vector3* set(const Vector3 &other) {
+    coord[0] = other.coord[0];
+    coord[1] = other.coord[1];
+    return this;
+  }
+
+  Vector3* set(double c) {
+    coord[0] = c;
+    coord[1] = c;
+    return this;
+  }
+
+  Vector3* normalize() {
+    auto length = this->length();
+    coord[0] /= length;
+    coord[1] /= length;
+    return this;
+  }
+
+  Vector3* addScaledVector(Vector3 &v, double s) {
+    coord[0] += v.coord[0] * s;
+    coord[1] += v.coord[1] * s;
+    return this;
+  }
+
+  Vector3* sub(const Vector3 &other) {
+    coord[0] -= other.coord[0];
+    coord[1] -= other.coord[1];
+    return this;
+  }
+  Vector3* add(const Vector3 &other) {
+    coord[0] += other.coord[0];
+    coord[1] += other.coord[1];
+    return this;
+  }
+};
+
 
 class NotEnoughQuadSpaceException: public exception {};
 
